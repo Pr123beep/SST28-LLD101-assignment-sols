7963e344f7742ad601c8adb42444a6ce026bbc90
Pr123beep <prathamjainfzr2006@gmail.com>
2026-02-23 02:46:08 +0530

    Ex5 done

--- SOLID/Ex5/src/Exporter.java
+++ SOLID/Ex5/src/Exporter.java
@@ -1,4 +1,18 @@
 public abstract class Exporter {
     // implied "contract" but not enforced (smell)
-    public abstract ExportResult export(ExportRequest req);
+    public final ExportResult export(ExportRequest req)
+    {
+        if(req == null) throw new IllegalArgumentException("Request cannot be null");
+
+        String title, body;
+
+        if(req.title == null) title = "";
+        else title = req.title;
+
+        if(req.body == null) body = "";
+        else body = req.body;
+        
+        return doExport(new ExportRequest(title, body));
+    }
+    protected abstract ExportResult doExport(ExportRequest req);
 }
--- SOLID/Ex5/src/PdfExporter.java
+++ SOLID/Ex5/src/PdfExporter.java
@@ -2,9 +2,8 @@ import java.nio.charset.StandardCharsets;
 
 public class PdfExporter extends Exporter {
     @Override
-    public ExportResult export(ExportRequest req) {
-        // LSP violation: tightens precondition arbitrarily
-        if (req.body != null && req.body.length() > 20) {
+    public ExportResult doExport(ExportRequest req) {
+        if (req.body.length() > 20) {
             throw new IllegalArgumentException("PDF cannot handle content > 20 chars");
         }
         String fakePdf = "PDF(" + req.title + "):" + req.body;
